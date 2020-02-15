ef48b11545229b1d836b26b7a85af558ccd9feed
justinli-code <61083469+justinli-code@users.noreply.github.com>
2020-02-15 20:25:44 +0800

    Create house.py

---
+++ house.py
@@ -0,0 +1,16 @@
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.fd(-200)
+t.fd(400)
+t.right(90)#第一次右转
+t.fd(200)
+t.right(90)#第二次右转
+t.fd(400)
+t.right(90)#第三次右转
+t.fd(200)
+t.right(45)#第四次右转
+t.fd(275)
+t.right(90)#第五次右转
+t.fd(275)
+turtle.done()
