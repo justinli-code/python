88f63753aea713bfbaeaa990236938864cba851f
justinli-code <61083469+justinli-code@users.noreply.github.com>
2020-03-01 16:08:05 +0800

    Create pingfen.py

---
+++ pingfen.py
@@ -0,0 +1,11 @@
+score=float(input("你得了多少分？："))
+if score>=101:
+    print("你骗谁呢！")
+elif score>=90:
+    print("真厉害！")
+elif score>=80:
+    print("还不错嘛！")
+elif score>=60:
+    print("咦，才刚及格！")
+else:
+    print("切，竟然没及格！")    
